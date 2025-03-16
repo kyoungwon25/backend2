02b19a9d6e786ba9f99e7ab928b9d6725b7e4074
kyoungwon25 <popodx54@gmail.com>
2025-03-16 13:11:13 +0900

    feature1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 public class Main {
 	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("aaaa");
+		System.out.println(sb.toString());
 		System.out.println("Hello, World!");
 	}
 }
